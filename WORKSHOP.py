f21b880d6d5cf4adf06ef6a46a536d1699d9c105
Victor Delamonica <victor.delamonica@epitech.eu>
2023-02-07 17:32:20 +0100

    Add getter for movies title

--- WORKSHOP.py
+++ WORKSHOP.py
@@ -14,12 +14,23 @@ def beautiful_requests(url):
 def exercise_1(url_exercise1):
     response = beautiful_requests(url_exercise1)
     line = response.findAll('img')
-    print(f"My image: {line[4]['src']}")
+    print(f"My image: {line[4]['src']} //")
 
 # Exercise 2
 # Extrais les titres des 250 meilleurs films de tous les temps
 def exercise_2(url_exercise2):
     response = beautiful_requests(url_exercise2)
+    all_links = response.findAll('a')
+    # res = all_links
+    for movie in all_links:
+        try:
+            x = movie['role']
+            continue
+        except:
+            try:
+                print(movie['title'])
+            except:
+                continue
 
 
 # Exercise 3
@@ -33,6 +44,9 @@ if __name__ == '__main__':
     url_exercise1 = 'https://github.com/VictorDelamonica/'
     url_exercise2 = 'https://www.imdb.com/chart/top/'
     url_exercise3 = 'https://fr.wikipedia.org/wiki/Liste_des_pays_par_PIB_nominal'
+    print("Exercise 1: ")
     exercise_1(url_exercise1)
-    # exercise_2(url_exercise2)
+    print("\n\nExercise 2: ")
+    exercise_2(url_exercise2)
+    print("\n\nExercise 3: ")
     # exercise_3(url_exercise3)
